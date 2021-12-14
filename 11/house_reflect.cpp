4d41f5cb4f55a90387c014fbc7f1cbdc3720f4bb
kushagrasirothia <42067055+kushagrasirothia@users.noreply.github.com>
2021-12-14 09:22:06 +0530

    Update house_reflect.cpp

--- 11/house_reflect.cpp
+++ 11/house_reflect.cpp
@@ -1,55 +1,57 @@
 #include<stdio.h>
 #include<math.h>
 #include<GL/glut.h>
-GLfloat house[2][9]={{100.0,100.0,175.0,250.0,250.0,150.0,150.0,200.0,200.0},        {100.0,300.0,400.0,300.0,100.0,100.0,150.0,150.0,100.0}};
-GLfloat theta,m,c;
-GLfloat h=100.0;
-GLfloat k=100.0;
+#include<iostream>
+using namespace std;
+GLfloat house[2][9] = { {100.0,100.0,175.0,250.0,250.0,150.0,150.0,200.0,200.0},        {100.0,300.0,400.0,300.0,100.0,100.0,150.0,150.0,100.0} };
+GLfloat theta, m, c;
+GLfloat h = 100.0;
+GLfloat k = 100.0;
 void drawhouse()
 {
-    glColor3f(1.0,0.0,0.0);
+    glColor3f(1.0, 0.0, 0.0);
     glBegin(GL_LINE_LOOP);
-    glVertex2f(house[0][0],house[1][0]);
-    glVertex2f(house[0][1],house[1][1]);
-    glVertex2f(house[0][3],house[1][3]);
-    glVertex2f(house[0][4],house[1][4]);
+    glVertex2f(house[0][0], house[1][0]);
+    glVertex2f(house[0][1], house[1][1]);
+    glVertex2f(house[0][3], house[1][3]);
+    glVertex2f(house[0][4], house[1][4]);
     glEnd();
-    glColor3f(1.0,0.0,1.0);
+    glColor3f(1.0, 0.0, 1.0);
     glBegin(GL_LINE_LOOP);
-    glVertex2f(house[0][5],house[1][5]);
-    glVertex2f(house[0][6],house[1][6]);
-    glVertex2f(house[0][7],house[1][7]);
-    glVertex2f(house[0][8],house[1][8]);
+    glVertex2f(house[0][5], house[1][5]);
+    glVertex2f(house[0][6], house[1][6]);
+    glVertex2f(house[0][7], house[1][7]);
+    glVertex2f(house[0][8], house[1][8]);
     glEnd();
-    glColor3f(1.0,0.0,0.0);
+    glColor3f(1.0, 0.0, 0.0);
     glBegin(GL_LINE_LOOP);
-    glVertex2f(house[0][1],house[1][1]);
-    glVertex2f(house[0][2],house[1][2]);
-    glVertex2f(house[0][3],house[1][3]);
+    glVertex2f(house[0][1], house[1][1]);
+    glVertex2f(house[0][2], house[1][2]);
+    glVertex2f(house[0][3], house[1][3]);
     glEnd();
 }
 void display()
 {
     int i;
-	float sin1=sin(theta);
-	float cos1=cos(theta);
-	float x1=0;
-	float x2=500;
-	float y1=m*x1+c;
-	float y2=m*x2+c;
-	
+    float sin1 = sin(theta);
+    float cos1 = cos(theta);
+    float x1 = 0;
+    float x2 = 500;
+    float y1 = m * x1 + c;
+    float y2 = m * x2 + c;
+
     GLfloat m[16];
-    for(i=0;i<15;i++)
-        m[i]=0.0;
-    m[0]=cos1*cos1-sin1*sin1;
-    m[1]=2*sin1*cos1;
-    m[4]=2*sin1*cos1;
-    m[5]=sin1*sin1-cos1*cos1;
-    m[12]=2*(-c)*cos1*sin1;
-    m[13]=(-c)*(sin1*sin1-cos1*cos1)+c;
-    m[10]=1;
-    m[15]=1;
-// yahan se bhi nhi aaya
+    for (i = 0;i < 15;i++)
+        m[i] = 0.0;
+    m[0] = cos1 * cos1 - sin1 * sin1;
+    m[1] = 2 * sin1 * cos1;
+    m[4] = 2 * sin1 * cos1;
+    m[5] = sin1 * sin1 - cos1 * cos1;
+    m[12] = 2 * (-c) * cos1 * sin1;
+    m[13] = (-c) * (sin1 * sin1 - cos1 * cos1) + c;
+    m[10] = 1;
+    m[15] = 1;
+    // yahan se bhi nhi aaya
     glMatrixMode(GL_MODELVIEW);
     glClear(GL_COLOR_BUFFER_BIT);
     drawhouse();
@@ -58,36 +60,36 @@ void display()
     drawhouse();
     glPopMatrix();
 
-	glColor3f(1.0,0.0,0.0);
-	glBegin(GL_LINES);
-	glVertex2f(x1,y1);
-	glVertex2f(x2,y2);
-	glEnd();
+    glColor3f(1.0, 0.0, 0.0);
+    glBegin(GL_LINES);
+    glVertex2f(x1, y1);
+    glVertex2f(x2, y2);
+    glEnd();
 
     glFlush();
-	}
+}
 void myinit()
 {
-    glClearColor(1.0,1.0,1.0,1.0);
-    glColor3f(1.0,1.0,0.0);
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glColor3f(1.0, 1.0, 0.0);
     glMatrixMode(GL_PROJECTION);// yahan se nhi samjh aaya
     glLoadIdentity();
-    gluOrtho2D(-499.0,499.0,-499.0,499.0);
+    gluOrtho2D(-499.0, 499.0, -499.0, 499.0);
     glMatrixMode(GL_MODELVIEW);
 }
-int main(int argc,char **argv)
+int main(int argc, char** argv)
 {
 
-    printf("enter the value of m(y=mx+c):");
-    scanf("%f",&m);
-	printf("Enter the value of c:");
-	scanf("%f",&c);
+    cout << "enter the value of m(y=mx+c):\n";
+    cin>> m;
+    cout<<"Enter the value of c:";
+    cin>>c;
     //theta=theta*3.141/180;
-	theta=atan(m);
-    glutInit(&argc,argv);
-    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-    glutInitWindowSize(500,500);
-    glutInitWindowPosition(0,0);
+    theta = atan(m);
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(0, 0);
     glutCreateWindow("reflected house");
     glutDisplayFunc(display);
     myinit();
